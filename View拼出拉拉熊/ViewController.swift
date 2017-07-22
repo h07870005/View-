ae286e98976e071f49d1ff44b42cc27b5c0eeae2
h07870005 <h07870005@yahoo.com.tw>
2017-07-23 05:53:06 +0800

    Initial Commit

---
+++ View拼出拉拉熊/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  View拼出拉拉熊
+//
+//  Created by Huang Hao on 2017/7/23.
+//  Copyright © 2017年 Huang Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
